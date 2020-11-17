afe5028d20227feace6c2c43a29cf01da79a9b1d
WonHyeongSeok <royle730@naver.com>
2020-11-17 15:34:33 +0900

    this is first commit

---
+++ GitTestCode/GitTestCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GitTestCode
+{
+    class Program
+    {
+
+    
+        //현재 작업상황 보여주면서 하기
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Go Staging Area!");
+
+            Console.WriteLine(" new one");
+        }
+    }
+}
